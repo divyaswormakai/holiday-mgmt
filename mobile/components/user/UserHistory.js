4eadc879a4e2674ca35280991c48f7f422dbb1c9
Divyaswor Makai <makaidivya@gmail.com>
2021-06-05 18:41:50 +0545

    Request delete for user swipeable done

--- mobile/components/user/UserHistory.js
+++ mobile/components/user/UserHistory.js
@@ -1,9 +1,131 @@
+import { Ionicons } from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, ToastAndroid, View } from 'react-native';
+import { Animated, FlatList, Text, ToastAndroid, TouchableOpacity, View } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import axios from '../../utils/axios';
 import { COLORS } from '../../utils/constant';
 import { vh, vw } from '../../utils/viewport';
+
+const getBorderColor = (decisionStatus) => {
+	switch (decisionStatus) {
+		case "REJECTED": {
+			return COLORS.red;
+		}
+		case "ACCEPTED": {
+			return COLORS.primary;
+		}
+		default: {
+			return COLORS.gray;
+		}
+	}
+};
+
+const SwipeableView = ({ item, deleteRequest }) => {
+	const rightSwap = (progress, dragX) => {
+		const scale = dragX.interpolate({
+			inputRange: [0, 100],
+			outputRange: [0, 1],
+			extrapolate: "clamp",
+		});
+
+		return (
+			<TouchableOpacity
+				onPress={() => {
+					deleteRequest(item);
+				}}
+				activeOpacity={0.5}
+			>
+				<View
+					style={{
+						backgroundColor: "#FBC9D0",
+						width: 20 * vw,
+						height: "100%",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<Animated.Text
+						style={{
+							transform: [{ scale: scale }],
+						}}
+					>
+						<Ionicons
+							name="close-circle-outline"
+							color="white"
+							size={15 * vw}
+						/>
+					</Animated.Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+	return (
+		<Swipeable
+			renderLeftActions={rightSwap}
+			containerStyle={{ marginVertical: 1 * vh }}
+		>
+			<View
+				style={{
+					padding: "5%",
+					// height: 65 * vh,
+					borderWidth: 2,
+					borderColor: getBorderColor(item.decisionStatus),
+					// marginVertical: 2 * vh,
+					flexDirection: "column",
+				}}
+			>
+				<View style={{ flexDirection: "row" }}>
+					<Text
+						style={{
+							fontWeight: "bold",
+							fontFamily: "",
+							fontSize: 3 * vw,
+						}}
+					>
+						Date:{" "}
+					</Text>
+					<Text>
+						{item.fromDate.slice(0, 10)} - {item.toDate.slice(0, 10)}
+					</Text>
+				</View>
+				<View style={{ flexDirection: "row" }}>
+					<Text style={{ fontWeight: "bold", fontFamily: "" }}>Reason: </Text>
+					<Text>{item?.reason || ""}</Text>
+				</View>
+				<View style={{ flexDirection: "row" }}>
+					<Text
+						style={{
+							fontWeight: "bold",
+							fontFamily: "",
+							fontSize: 4 * vw,
+						}}
+					>
+						Status:{" "}
+					</Text>
+					<Text
+						style={{
+							fontWeight: "bold",
+							fontFamily: "",
+							fontSize: 4 * vw,
+							color: getBorderColor(item.decisionStatus),
+						}}
+					>
+						{item.decisionStatus}
+					</Text>
+				</View>
+				{item.rejectionReason && (
+					<View style={{ flexDirection: "row" }}>
+						<Text style={{ fontWeight: "bold", fontFamily: "" }}>
+							Rejection reason:{" "}
+						</Text>
+						<Text>{item?.rejectionReason || ""}</Text>
+					</View>
+				)}
+			</View>
+		</Swipeable>
+	);
+};
 
 const UserHistory = ({ userID }) => {
 	const [requestHistoryList, setRequestHistoryList] = useState([]);
@@ -28,17 +150,22 @@ const UserHistory = ({ userID }) => {
 		}
 	};
 
-	const getBorderColor = (decisionStatus) => {
-		switch (decisionStatus) {
-			case "REJECTED": {
-				return COLORS.red;
+	const deleteRequest = async (request) => {
+		try {
+			console.log(request.id);
+			const result = await axios.delete(`user/delete-request/${request.id}`);
+			if (result.status !== 200) {
+				throw new Error(result);
 			}
-			case "ACCEPTED": {
-				return COLORS.primary;
-			}
-			default: {
-				return COLORS.gray;
-			}
+			setRequestHistoryList(
+				requestHistoryList.filter((item) => item.id !== request.id)
+			);
+		} catch (err) {
+			console.log(err.response);
+			ToastAndroid.show(
+				err?.response?.data?.error || err?.message || "Error",
+				ToastAndroid.SHORT
+			);
 		}
 	};
 
@@ -49,58 +176,7 @@ const UserHistory = ({ userID }) => {
 				keyExtractor={(item) => `Request-History-${item.id}`}
 				showsVerticalScrollIndicator={false}
 				renderItem={({ item }) => (
-					<View
-						style={{
-							padding: "5%",
-							// height: 65 * vh,
-							borderWidth: 2,
-							borderColor: getBorderColor(item.decisionStatus),
-							marginVertical: 2 * vh,
-							flexDirection: "column",
-						}}
-					>
-						<View style={{ flexDirection: "row" }}>
-							<Text
-								style={{ fontWeight: "bold", fontFamily: "", fontSize: 3 * vw }}
-							>
-								Date:{" "}
-							</Text>
-							<Text>
-								{item.fromDate.slice(0, 10)} - {item.toDate.slice(0, 10)}
-							</Text>
-						</View>
-						<View style={{ flexDirection: "row" }}>
-							<Text style={{ fontWeight: "bold", fontFamily: "" }}>
-								Reason:{" "}
-							</Text>
-							<Text>{item?.reason || ""}</Text>
-						</View>
-						<View style={{ flexDirection: "row" }}>
-							<Text
-								style={{ fontWeight: "bold", fontFamily: "", fontSize: 4 * vw }}
-							>
-								Status:{" "}
-							</Text>
-							<Text
-								style={{
-									fontWeight: "bold",
-									fontFamily: "",
-									fontSize: 4 * vw,
-									color: getBorderColor(item.decisionStatus),
-								}}
-							>
-								{item.decisionStatus}
-							</Text>
-						</View>
-						{item.rejectionReason && (
-							<View style={{ flexDirection: "row" }}>
-								<Text style={{ fontWeight: "bold", fontFamily: "" }}>
-									Rejection reason:{" "}
-								</Text>
-								<Text>{item?.rejectionReason || ""}</Text>
-							</View>
-						)}
-					</View>
+					<SwipeableView item={item} deleteRequest={deleteRequest} />
 				)}
 			/>
 		</View>
